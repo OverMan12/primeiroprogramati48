3080ed0a30ab1e8b9658ed0119985705c59e6c09
Pablo <overmanplays2021@email.com>
2025-11-11 16:32:10 -0300

    primeira versão da Calculadora

--- primeiroprogramati48/Form1.Designer.cs
+++ primeiroprogramati48/Form1.Designer.cs
@@ -28,35 +28,117 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
-            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.Titulo = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.Mais = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
-            // button1
+            // textBox1
             // 
-            this.button1.Location = new System.Drawing.Point(202, 138);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(104, 48);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.textBox1.BackColor = System.Drawing.SystemColors.Menu;
+            this.textBox1.ImeMode = System.Windows.Forms.ImeMode.Disable;
+            this.textBox1.Location = new System.Drawing.Point(78, 170);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(143, 20);
+            this.textBox1.TabIndex = 1;
             // 
-            // label1
+            // textBox2
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(459, 93);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 13);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "label1";
+            this.textBox2.BackColor = System.Drawing.SystemColors.Menu;
+            this.textBox2.ImeMode = System.Windows.Forms.ImeMode.Disable;
+            this.textBox2.Location = new System.Drawing.Point(330, 170);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(143, 20);
+            this.textBox2.TabIndex = 2;
+            // 
+            // Titulo
+            // 
+            this.Titulo.Font = new System.Drawing.Font("ISOCPEUR", 36F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Titulo.Location = new System.Drawing.Point(143, 42);
+            this.Titulo.Name = "Titulo";
+            this.Titulo.Size = new System.Drawing.Size(250, 100);
+            this.Titulo.TabIndex = 3;
+            this.Titulo.Text = "Caluladora";
+            this.Titulo.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.Titulo.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.BackColor = System.Drawing.SystemColors.Menu;
+            this.textBox3.ImeMode = System.Windows.Forms.ImeMode.Disable;
+            this.textBox3.Location = new System.Drawing.Point(78, 243);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(395, 20);
+            this.textBox3.TabIndex = 4;
+            // 
+            // Mais
+            // 
+            this.Mais.BackColor = System.Drawing.SystemColors.Menu;
+            this.Mais.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Mais.ImeMode = System.Windows.Forms.ImeMode.Disable;
+            this.Mais.Location = new System.Drawing.Point(78, 308);
+            this.Mais.Name = "Mais";
+            this.Mais.Size = new System.Drawing.Size(51, 47);
+            this.Mais.TabIndex = 5;
+            this.Mais.Text = "+";
+            this.Mais.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox4
+            // 
+            this.textBox4.BackColor = System.Drawing.SystemColors.Menu;
+            this.textBox4.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox4.ImeMode = System.Windows.Forms.ImeMode.Disable;
+            this.textBox4.Location = new System.Drawing.Point(181, 308);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(51, 47);
+            this.textBox4.TabIndex = 6;
+            this.textBox4.Text = "-";
+            this.textBox4.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox5
+            // 
+            this.textBox5.BackColor = System.Drawing.SystemColors.Menu;
+            this.textBox5.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox5.ImeMode = System.Windows.Forms.ImeMode.Disable;
+            this.textBox5.Location = new System.Drawing.Point(290, 308);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(51, 47);
+            this.textBox5.TabIndex = 7;
+            this.textBox5.Text = "X";
+            this.textBox5.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox6
+            // 
+            this.textBox6.BackColor = System.Drawing.SystemColors.Menu;
+            this.textBox6.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox6.ImeMode = System.Windows.Forms.ImeMode.Disable;
+            this.textBox6.Location = new System.Drawing.Point(401, 308);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(51, 47);
+            this.textBox6.TabIndex = 8;
+            this.textBox6.Text = "/";
+            this.textBox6.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.button1);
+            this.ClientSize = new System.Drawing.Size(543, 420);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.Mais);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.Titulo);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.ForeColor = System.Drawing.Color.CornflowerBlue;
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -66,8 +148,15 @@
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label Titulo;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.TextBox Mais;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
     }
 }
 
--- primeiroprogramati48/Form1.cs
+++ primeiroprogramati48/Form1.cs
@@ -16,5 +16,15 @@ namespace primeiroprogramati48
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
